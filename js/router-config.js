7c34f2b9a17fbba17596b3b2a02308956cd1143f
jacob kleiman <jacobakleiman@gmail.com>
2021-09-02 13:09:48 -0700

    initializes angular app config

---
+++ js/router-config.js
@@ -0,0 +1,13 @@
+var app = angular.module('BELEODAQ', ['ui.router']);
+var offsetHeight, footer;
+let menuToggled = false;
+
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.when('', '/');
+    $urlRouterProvider.otherwise('/');
+  
+    $locationProvider.html5Mode({
+      enabled: false,
+    });
+
+});
