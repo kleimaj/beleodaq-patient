daf0d9cef18016834cdb0b944545a5d5f9fd3598
jacob kleiman <jacobakleiman@gmail.com>
2021-09-07 13:06:34 -0700

    adds additional states to ui-router

--- js/router-config.js
+++ js/router-config.js
@@ -12,6 +12,18 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
         .state('LANDING-PAGE', {
             url: '/',
             templateUrl: 'landing-page.html'
-        });
+        })
+        .state('PTCL', {
+            url: '/about/ptcl',
+            templateUrl: 'content/about/ptcl.html'
+        })
+        .state('BELEODAQ', {
+            url: '/about/beleodaq',
+            templateUrl: 'content/about/beleodaq.html'
+        })
+        .state('WORKS', {
+            url: '/about/how-it-works',
+            templateUrl: 'content/about/works.html'
+        })
 
 });
