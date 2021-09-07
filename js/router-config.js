682f9cc122853da9c1532e1d1c6b86d9236409cc
jacob kleiman <jacobakleiman@gmail.com>
2021-09-07 16:32:01 -0700

    initializes taking html pages, router-config

--- js/router-config.js
+++ js/router-config.js
@@ -25,5 +25,20 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
             url: '/about/how-it-works',
             templateUrl: 'content/about/works.html'
         })
-
+        .state('DOSING', {
+            url: '/taking-beleodaq/dosing-schedule',
+            templateUrl: 'content/taking/dosing.html'
+        })
+        .state('EXPECT', {
+          url: '/taking-beleodaq/what-to-expect',
+          templateUrl: 'content/taking/expect.html'
+        })
+        .state('PREPARING', {
+          url: '/taking-beleodaq/preparing-for-treatment',
+          templateUrl: 'content/taking/preparing.html'
+        })
+        .state('MONITORING', {
+          url: '/taking-beleodaq/monitoring-your-treatment',
+          templateUrl: 'content/taking/monitoring.html'
+        })
 });
